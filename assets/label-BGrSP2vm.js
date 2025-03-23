01cc372515157bbf1d556658e33537b7133a087e
Adrian Escutia Soto <adrianescutia@yahoo.com.mx>
2025-03-22 19:11:42 -0500

    Updates

---
+++ assets/label-BGrSP2vm.js
@@ -0,0 +1,1 @@
+import{r,j as t}from"./react-vendor-BIgZFnT2.js";import{ac as a}from"./ui-components-CeV64dF6.js";import{a as m,l as i}from"./index-CXZKCkms.js";const l=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:m(l(),s),...e}));p.displayName=a.displayName;export{p as L};
